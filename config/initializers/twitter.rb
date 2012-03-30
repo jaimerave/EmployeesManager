9314c8e32fb82e2997324aab09862713f6847add
Jaime Rave <jaimerave@gmail.com>
2012-03-30 18:47:11 -0500

    Some tweaks

---
+++ config/initializers/twitter.rb
@@ -0,0 +1,6 @@
+Twitter.configure do |config|
+  config.consumer_key = "LLQc6QHkH7tJ7lljyLw"
+  config.consumer_secret = "pfdUYGOSzdxrM95Zy7CS7d7CbwxHho3d5JYbpnPDegU"
+  config.oauth_token = "16753644-Pln5FfyhS1QD9pJO9tfhk0vgllm9s82EhyQfO9Dsx"
+  config.oauth_token_secret = "i52ATZhimKeAH1iptt3U3OKcXgxOInLI77tPoDi7I8"
+end
